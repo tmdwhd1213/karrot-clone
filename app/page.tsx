a04c37d967e6fe5788d1cd3d38ad43e6ff25383d
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-06-26 17:02:45 +0900

    Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -24,17 +24,27 @@ export default function Home() {
                   <div className="w-20 h-4 rounded-full bg-gray-400" />
                 </div>
               ) : (
-                <div key={index} className="flex items-center gap-5">
+                <div key={index} className="flex items-center gap-5 group">
                   <div className="size-10 bg-blue-400 rounded-full" />
                   <span
                     className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full
-                  empty:animate-pulse empty:bg-gray-300"
+                  empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500"
                   >
                     {person}
                   </span>
                   <div className="size-6 bg-red-500 relative text-white flex items-center justify-center rounded-full">
                     <span className="z-10">{index}</span>
                     <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+                  </div>
+                  <div className="group flex flex-col">
+                    <input
+                      className="bg-gray-100 w-full"
+                      placeholder="Write your email"
+                    />
+                    <span className="group-focus-within:block hidden">
+                      Make sure it is a valid email...
+                    </span>
+                    <button>Submit</button>
                   </div>
                 </div>
               )}
