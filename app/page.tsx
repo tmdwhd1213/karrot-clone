0da028e964494d5216e48d72a8c0f935d8c67c3a
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-06-26 16:15:34 +0900

    has 선택자, 하위 자식 all선택자 (*)

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+      <div
+        className="bg-white w-full shadow-lg p-5 rounded-3xl 
+        max-w-screen-sm flex flex-col md:flex-row gap-2
+        *:outline-none ring ring-transparent transition-shadow
+        has-[:invalid]:ring-red-100"
+      >
         <input
-          className="w-full rounded-full py-3 h-10 bg-gray-200 pl-5 outline-none 
+          className="w-full rounded-full py-3 h-10 bg-gray-200 pl-5 
             ring ring-transparent focus:ring-green-500 focus:ring-offset-2
             transition-shadow placeholder:drop-shadow
             invalid:md:focus:ring-red-500 peer"
-          type="email"
+          type="text"
           required
           placeholder="Email address"
         />
@@ -19,7 +24,7 @@ export default function Home() {
         {/* 클래스네임을 커서로 갖다대어보면 --tw-??-?? 이런식으로 변수가 설정되어있음. 이걸 사용자가 임의로 가져가 쓸 수 있다. */}
         <button
           className="text-white py-2 rounded-full
-            active:scale-90 transition-transform font-medium focus:scale-90 outline-none
+            active:scale-90 transition-transform font-medium focus:scale-90
             bg-opacity-100 md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400
             peer-invalid:bg-red-100 peer-required:bg-green-500"
         >
