2e61dbf388d6ef2c019a26682f1c0423ec5f0993
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-06-26 15:45:25 +0900

    practice tailwind CSS

---
+++ app/components/Cards.tsx
@@ -0,0 +1,36 @@
+export default function Cards() {
+  return (
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
+              In transit
+            </span>
+            <span className="text-4xl font-semibold dark:text-white">
+              Coolblue
+            </span>
+          </div>
+          <div className="size-12 rounded-full bg-orange-400" />
+        </div>
+        <div className="my-2 flex items-center gap-2">
+          {/* modifier: 콜론 뒤가 참일 때 발동 */}
+          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 transition ">
+            Today
+          </span>
+          <span className="dark:text-gray-100">9:30-10:30</span>
+        </div>
+        <div className="relative">
+          <div className="bg-gray-200 absolute w-full h-2 rounded-full" />
+          <div className="bg-green-500 absolute w-2/3 h-2 rounded-full" />
+        </div>
+        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="bg-black"></main>;
+  return (
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+        <input
+          className="w-full rounded-full py-3 h-10 bg-gray-200 pl-5 outline-none 
+            ring ring-transparent focus:ring-orange-500 focus:ring-offset-2
+            transition-shadow placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here..."
+        />
+        {/* 클래스네임을 커서로 갖다대어보면 --tw-??-?? 이런식으로 변수가 설정되어있음. 이걸 사용자가 임의로 가져가 쓸 수 있다. */}
+        <button
+          className="bg-black text-white py-2 rounded-full
+            active:scale-90 transition-transform font-medium focus:scale-90 outline-none
+            bg-opacity-100 md:px-10"
+        >
+          Search
+        </button>
+      </div>
+    </main>
+  );
 }
