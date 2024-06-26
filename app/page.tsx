79fcf47bc35e88a9a32d62b25e5e35297ec39790
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-06-26 18:11:37 +0900

    create Home Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      className="flex flex-col items-center
+      justify-center min-h-screen p-6"
+    >
+      <div
+        className="my-auto flex flex-col
+          gap-2 items-center *:font-medium"
+      >
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white 
+          text-lg py-2.5 rounded-md text-center
+          hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline underline-offset-2">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
