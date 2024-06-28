c1d2218f045be9e76acf283fdef6a910d38bd718
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-06-28 18:50:48 +0900

    add SMS TOKEN & prisma add relate data onDelete: Cascade

---
+++ app/lib/db.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function test() {
+  const user = await db.user.findMany({
+    where: {
+      username: {
+        contains: "est",
+      },
+    },
+  });
+
+  console.log(user);
+}
+
+async function test2() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+
+  console.log(token);
+}
+test2();
+// test();
+
+export default db;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import "./lib/db";
+
 export default function Home() {
   return (
     <main
