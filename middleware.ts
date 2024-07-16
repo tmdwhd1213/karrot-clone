b0af3b4d82d7329fac08a249511dc22da2d694ed
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-07-16 20:49:34 +0900

    github-login

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,19 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./app/lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+  "/github/start": true,
+  "/github/complete": true,
+};
+
 export async function middleware(request: NextRequest) {
   // 방법 1. 모든 pathname에 if문을 써서 제외시키기.
   // const pathname = request.nextUrl.pathname;
@@ -13,10 +26,20 @@ export async function middleware(request: NextRequest) {
   // if (pathname === "/profile") {
   //   return Response.redirect(new URL("/", request.url));
   // }
-  console.log("hello!");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (exists) {
+      return NextResponse.redirect(new URL("/product", request.url));
+    }
+  }
 }
 
 // 방법 2. config로 matcher에 해당하는 것들을 제외시키기.
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
