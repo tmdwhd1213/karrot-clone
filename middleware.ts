d160f312b37e5100a4cadda3fad1e7828558e906
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-07-16 18:29:12 +0900

    middleware & config.matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,22 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./app/lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+  // 방법 1. 모든 pathname에 if문을 써서 제외시키기.
+  // const pathname = request.nextUrl.pathname;
+  // if (pathname === "/") {
+  //   const response = NextResponse.next();
+  //   response.cookies.set("middleware-cookie", "hello!");
+  //   return response;
+  // }
+  // if (pathname === "/profile") {
+  //   return Response.redirect(new URL("/", request.url));
+  // }
+  console.log("hello!");
 }
+
+// 방법 2. config로 matcher에 해당하는 것들을 제외시키기.
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
