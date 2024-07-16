8e76f6a8f680ce7df9d39fdda1b1eda5bd7e40c6
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-07-16 18:15:42 +0900

    logout & middleware(Response<Fetch API>)

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,11 +1,15 @@
 "use server";
 
 import { z } from "zod";
+import bcrypt from "bcrypt";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "../lib/constants";
+import db from "../lib/db";
+import { redirect } from "next/navigation";
+import getSession from "../lib/session";
 
 const formSchema = z.object({
   email: z.string().email().toLowerCase(),
@@ -27,7 +31,32 @@ export async function login(prevState: any, formData: FormData) {
     console.log(result.error.flatten());
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "xxxx"
+    );
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["잘못된 비밀번호입니다."],
+          email: [],
+        },
+      };
+    }
   }
   // input에 name 어트리뷰트를 설정해야지 FormData 객체가 바라볼 수 있음.
 }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,38 @@
-export default function Profile() {
-  return <h1>welcome to your profile</h1>;
+import { notFound, redirect } from "next/navigation";
+import db from "../lib/db";
+import getSession from "../lib/session";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    // 쿠기 없애는 작업 -> 로그아웃
+    session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>welcome! {user?.username}</h1>
+      {/* form안에 버튼 하나만 있다면 "use client"없이 action으로 onClick 작업 가능 */}
+      <form action={logOut}>
+        <button>로그아웃</button>
+      </form>
+    </div>
+  );
 }
