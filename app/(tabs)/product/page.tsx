15ce74cedcc5602323fc9d380e8466218d518e0d
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-07-22 17:07:23 +0900

    skeleton the products & pseudo images

--- app/(tabs)/product/page.tsx
+++ app/(tabs)/product/page.tsx
@@ -1,7 +1,27 @@
-export default function Chat() {
+import ListProduct from "@/app/components/list-product";
+import db from "@/app/lib/db";
+
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+
+  return products;
+}
+
+export default async function Products() {
+  const products = await getProducts();
   return (
-    <div>
-      <h1 className="text-white text-4xl">물건!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
     </div>
   );
 }
