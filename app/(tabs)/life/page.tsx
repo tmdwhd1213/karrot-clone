2aa41bf50d713e963a194ee5a43a7f86794bfd66
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-08-04 16:35:25 +0900

    life skeleton

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,4 +1,9 @@
-export default function Chat() {
+async function getPosts() {
+  await new Promise((r) => setTimeout(r, 3000));
+}
+
+export default async function Life() {
+  const posts = await getPosts(); // 스켈레톤
   return (
     <div>
       <h1 className="text-white text-4xl">동네생활!</h1>
