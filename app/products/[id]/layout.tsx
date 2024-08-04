dca35860bf258cc09f72125a827dd1309febf97d
Seung-Jong Oh <71041956+tmdwhd1213@users.noreply.github.com>
2024-08-04 16:30:28 +0900

    post_like_comment

--- app/products/[id]/layout.tsx
+++ app/products/[id]/layout.tsx
@@ -1,16 +1,7 @@
-import UpperTabBar from "@/components/upper-tab-bar";
-import { getIsOwner } from "./page";
-
 export default async function TabLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const isOwner = await getIsOwner();
-  return (
-    <div>
-      {/* <UpperTabBar /> */}
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    serverActions: true,
-  },
 };
 
 export default nextConfig;
