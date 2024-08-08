590365e493e16779cff24e910dbfc8b18462314b
Seung-Jong Oh <tmdwhd319@gmail.com>
2024-08-08 11:42:25 +0900

    input color change

--- components/input.tsx
+++ components/input.tsx
@@ -14,7 +14,7 @@ const Input = ({
   <div className="flex flex-col gap-2">
     <input
       name={name}
-      className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+      className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-green-500 border-none placeholder:text-neutral-400"
       {...rest}
     />
     {errors.map((error, index) => (
